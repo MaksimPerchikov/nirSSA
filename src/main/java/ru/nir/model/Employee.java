d52b35c2ce6aee74ce294bf472108167d20e12bb
Maksim Perchikov <MPerchikov@ibs.ru>
2022-12-08 20:37:08 +0300

    Моделька

---
+++ src/main/java/ru/nir/model/Employee.java
@@ -0,0 +1,24 @@
+package ru.nir.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+}
